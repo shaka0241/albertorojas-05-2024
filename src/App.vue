f273d568e35e6927523d817185396cde853b8efc
Alberto Rojas <alberto.erg87@gmail.com>
2024-06-02 11:16:06 -0500

    first steps

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,18 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
   </div>
 
+  <RouterView /> -->
+  <header class="flex justify-between">
+    <img alt="Vue logo" src="./assets/logo.svg" width="80" height="80" />
+    <nav class="flex gap-4 items-start">
+      <RouterLink to="/" class="hover:border-b border-[--main-color-text]">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup></script>
 
 <template>
-  <div>Hola mundo</div>
+  <div class="home">
+    <h1>Hola mundo</h1>
+  </div>
 </template>
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
